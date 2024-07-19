19fee60b009a38821488dba7e00d7d50eb6148b5
YoumnaAmin <youmnamin564@gmail.com>
2024-07-19 04:03:39 +0300

    landing page

---
+++ app/static/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const features = document.querySelectorAll('.feature');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    features.forEach(feature => {
+        observer.observe(feature);
+    });
+});
